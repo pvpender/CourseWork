4e71b910b69ead424c972adfb80552ebb335be86
pvpender <Nicolaymoiseev1@gmail.com>
2021-12-08 22:07:31 +0300

    Добавьте файлы проекта.

---
+++ Курсач/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Курсач
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(180, 63);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(489, 302);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.plotView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Курсач/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace Курсач
+{
+    public partial class Form1 : Form
+    {
+        private PlotModel plot;
+        public Form1()
+        {
+            InitializeComponent();
+            plot = new PlotModel { Title = "" };
+            plot.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.1));
+            this.plotView1.Model = plot;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.plotView1.Model.Series.Clear();
+            plot.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1));
+            this.plotView1.InvalidatePlot(true);
+        }
+
+
+    }
+}
