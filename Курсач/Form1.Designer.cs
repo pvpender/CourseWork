e1348e22f57f7fd05c727f2a82224bcc9f423d8e
pvpender <Nicolaymoiseev1@gmail.com>
2021-12-09 13:11:07 +0300

    ch

--- Курсач/Form1.Designer.cs
+++ Курсач/Form1.Designer.cs
@@ -31,13 +31,19 @@ namespace Курсач
         {
             this.plotView1 = new OxyPlot.WindowsForms.PlotView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // plotView1
             // 
-            this.plotView1.Location = new System.Drawing.Point(180, 63);
+            this.plotView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.plotView1.BackColor = System.Drawing.Color.AliceBlue;
+            this.plotView1.Location = new System.Drawing.Point(197, 36);
             this.plotView1.Name = "plotView1";
             this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.plotView1.Size = new System.Drawing.Size(489, 302);
             this.plotView1.TabIndex = 0;
             this.plotView1.Text = "plotView1";
@@ -47,7 +53,7 @@ namespace Курсач
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(288, 371);
+            this.button1.Location = new System.Drawing.Point(310, 371);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(257, 67);
             this.button1.TabIndex = 1;
@@ -55,15 +61,69 @@ namespace Курсач
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button2.Location = new System.Drawing.Point(856, -2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "○";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(825, -2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "○";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.Color.Lime;
+            this.button4.Location = new System.Drawing.Point(794, -2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(25, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "○";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(81)))));
+            this.ClientSize = new System.Drawing.Size(878, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.plotView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
@@ -72,6 +132,9 @@ namespace Курсач
 
         private OxyPlot.WindowsForms.PlotView plotView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Курсач/Form1.cs
+++ Курсач/Form1.cs
@@ -14,23 +14,68 @@ namespace Курсач
 {
     public partial class Form1 : Form
     {
-        private PlotModel plot;
+        private PlotModel _plot;
+        private bool _dragging = false;
+        private Point _dragCursorPoint;
+        private Point _dragFormPoint;
         public Form1()
         {
             InitializeComponent();
-            plot = new PlotModel { Title = "" };
-            plot.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.1));
-            this.plotView1.Model = plot;
-            
+            _plot = new PlotModel { Title = "" };
+            _plot.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.1));
+            this.plotView1.Model = _plot;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             this.plotView1.Model.Series.Clear();
-            plot.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1));
+            _plot.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1));
             this.plotView1.InvalidatePlot(true);
         }
 
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;
+            _dragCursorPoint = Cursor.Position;
+            _dragFormPoint = this.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(_dragCursorPoint));
+                this.Location = Point.Add(_dragFormPoint, new Size(dif));
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
 
     }
 }
